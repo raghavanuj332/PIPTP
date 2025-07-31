06cd71bfd8c0d2233eacf016c854b6c0808d0f50
raghavanuj332 <141630151+raghavanuj332@users.noreply.github.com>
2025-07-31 13:50:06 +0530

    Add files via upload

---
+++ lec1pro2.java
@@ -0,0 +1,11 @@
+public class lec1pro2 {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if (x > 0 && x % 2 != 0) {
+            System.out.println("Positive odd");
+        } else {
+            System.out.println("Other");
+        }
+    }
+}
